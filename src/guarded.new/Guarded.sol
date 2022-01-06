74ec3bbd123f4b28fd0ce137bd07a6f82c252218
Daniel Luca (@CleanUnicorn) <cleanunicorn@protonmail.com>
2022-01-06 11:06:02 +0000

    🤹‍♀ Temporarily move guarded to old/new to make sure integration happens explicitly

---
+++ src/guarded.new/Guarded.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.0;
+
+/// @title Guarded
+/// @notice Mixin implementing an authentication scheme on a method level
+abstract contract Guarded {
+    /// ======== Custom Errors ======== ///
+
+    error Guarded__notRoot();
+    error Guarded__notGranted();
+
+    /// ======== Storage ======== ///
+
+    /// @notice Wildcard for granting a caller to call every guarded method
+    bytes32 public constant ANY_SIG = keccak256("ANY_SIG");
+
+    /// @notice Mapping storing who is granted to which method
+    /// @dev Method Signature => Caller => Bool
+    mapping(bytes32 => mapping(address => bool)) private _canCall;
+
+    /// ======== Events ======== ///
+
+    event AllowCaller(bytes32 sig, address who);
+    event BlockCaller(bytes32 sig, address who);
+
+    constructor() {
+        // set root
+        _canCall[ANY_SIG][msg.sender] = true;
+        emit AllowCaller(ANY_SIG, msg.sender);
+    }
+
+    /// ======== Auth ======== ///
+
+    modifier callerIsRoot() {
+        if (canCall(ANY_SIG, msg.sender)) {
+            _;
+        } else revert Guarded__notRoot();
+    }
+
+    modifier checkCaller() {
+        if (canCall(msg.sig, msg.sender)) {
+            _;
+        } else revert Guarded__notGranted();
+    }
+
+    /// @notice Grant the right to call method `sig` to `who`
+    /// @dev Only the root user (granted `ANY_SIG`) is able to call this method
+    /// @param sig Method signature (4Byte)
+    /// @param who Address of who should be able to call `sig`
+    function allowCaller(bytes32 sig, address who) external callerIsRoot {
+        _canCall[sig][who] = true;
+        emit AllowCaller(sig, who);
+    }
+
+    /// @notice Revoke the right to call method `sig` from `who`
+    /// @dev Only the root user (granted `ANY_SIG`) is able to call this method
+    /// @param sig Method signature (4Byte)
+    /// @param who Address of who should not be able to call `sig` anymore
+    function blockCaller(bytes32 sig, address who) external callerIsRoot {
+        _canCall[sig][who] = false;
+        emit BlockCaller(sig, who);
+    }
+
+    /// @notice Returns if `who` can call `sig`
+    /// @param sig Method signature (4Byte)
+    /// @param who Address of who should be able to call `sig`
+    function canCall(bytes32 sig, address who) public view returns (bool) {
+        return (_canCall[sig][who] || _canCall[ANY_SIG][who]);
+    }
+}
---
+++ src/guarded.new/Guarded.t.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import {DSTest} from "ds-test/test.sol";
+
+import {Caller} from "src/test/utils/Caller.sol";
+
+import {Guarded} from "./Guarded.sol";
+
+contract GuardedInstance is Guarded {
+    constructor() Guarded() {}
+
+    function guardedMethod() external checkCaller {}
+
+    function guardedMethodRoot() external checkCaller {}
+}
+
+contract GuardedTest is DSTest {
+    GuardedInstance guarded;
+
+    function setUp() public {
+        guarded = new GuardedInstance();
+    }
+
+    function test_custom_role() public {
+        Caller user = new Caller();
+        bool ok;
+        bool canCall;
+
+        // Should not be able to call method
+        (ok, ) = user.externalCall(address(guarded), abi.encodeWithSelector(guarded.guardedMethod.selector));
+        assertTrue(ok == false, "Cannot call guarded method before adding permissions");
+
+        // Adding permission should allow user to call method
+        guarded.allowCaller(guarded.guardedMethod.selector, address(user));
+        (ok, ) = user.externalCall(address(guarded), abi.encodeWithSelector(guarded.guardedMethod.selector));
+        assertTrue(ok, "Can call method after adding permissions");
+
+        // User has custom permission to call method
+        canCall = guarded.canCall(guarded.guardedMethod.selector, address(user));
+        assertTrue(canCall, "User has permission");
+
+        // Removing role disables permission
+        guarded.blockCaller(guarded.guardedMethod.selector, address(user));
+        (ok, ) = user.externalCall(address(guarded), abi.encodeWithSelector(guarded.guardedMethod.selector));
+        assertTrue(ok == false, "Cannot call method after removing permissions");
+
+        // User does not have custom role
+        canCall = guarded.canCall(guarded.guardedMethod.selector, address(user));
+        assertTrue(canCall == false, "User does not have permission");
+    }
+
+    function test_root_role() public {
+        Caller user = new Caller();
+        bool ok;
+
+        // Should not be able to call method
+        (ok, ) = user.externalCall(address(guarded), abi.encodeWithSelector(guarded.guardedMethodRoot.selector));
+        assertTrue(ok == false, "Root can call method");
+
+        // Adding ANY_SIG should allow user to call method
+        guarded.allowCaller(guarded.ANY_SIG(), address(user));
+        (ok, ) = user.externalCall(address(guarded), abi.encodeWithSelector(guarded.guardedMethodRoot.selector));
+        assertTrue(ok, "User can call method after adding root permissions");
+
+        // User has senatus role
+        bool canCall = guarded.canCall(guarded.ANY_SIG(), address(user));
+        assertTrue(canCall, "User has permission");
+
+        // Removing senatus role disables permission
+        guarded.blockCaller(guarded.ANY_SIG(), address(user));
+        (ok, ) = user.externalCall(address(guarded), abi.encodeWithSelector(guarded.guardedMethodRoot.selector));
+        assertTrue(ok == false, "Senatus cannot call method after removing permissions");
+
+        // User does not have senatus role
+        canCall = guarded.canCall(guarded.ANY_SIG(), address(user));
+        assertTrue(canCall == false, "User does not have role");
+    }
+
+    function test_root_has_god_mode_access() public {
+        Caller user = new Caller();
+        bool ok;
+
+        // Should not be able to call method
+        (ok, ) = user.externalCall(address(guarded), abi.encodeWithSelector(guarded.guardedMethod.selector));
+        assertTrue(ok == false, "Cannot call guarded method before adding permissions");
+
+        // Adding senatus role should allow user to call method
+        guarded.allowCaller(guarded.ANY_SIG(), address(user));
+        (ok, ) = user.externalCall(address(guarded), abi.encodeWithSelector(guarded.guardedMethod.selector));
+        assertTrue(ok, "Can call method after adding senatus role");
+    }
+}
