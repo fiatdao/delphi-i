8d2ba9713e783e2094556e143fd3993a73c9d92b
Alexandru Marcu <alx.marcu@gmail.com>
2022-01-09 19:38:59 +0200

    lint contracts

--- src/valueprovider/NotionalFinance/NotionalFinanceValueProvider.sol
+++ src/valueprovider/NotionalFinance/NotionalFinanceValueProvider.sol
@@ -4,10 +4,7 @@ pragma solidity ^0.8.0;
 import {IValueProvider} from "../IValueProvider.sol";
 
 contract NotionalFinanceValueProvider is IValueProvider {
-    constructor()
-    {
-
-    }
+    constructor() {}
 
     function value() external view override(IValueProvider) returns (int256) {
         return 0;
