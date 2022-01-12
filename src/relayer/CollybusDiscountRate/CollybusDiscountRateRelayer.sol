110da6153fdfb9d92e300328847fcc4d7d97d241
Alexandru Marcu <alx.marcu@gmail.com>
2022-01-12 17:51:18 +0200

    WIP collybus discount rate relayer

--- src/relayer/CollybusDiscountRate/CollybusDiscountRateRelayer.sol
+++ src/relayer/CollybusDiscountRate/CollybusDiscountRateRelayer.sol
@@ -1,38 +1,117 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import {IRelayer} from "../IRelayer.sol";
+import {IRelayer} from "src/relayer/IRelayer.sol";
+import {IOracle} from "src/oracle/IOracle.sol";
+import {ICollybus} from "src/relayer/ICollybus.sol";
 
-interface ICollybus {
-    function updateDiscountRate(uint256 tokenId, uint256 rate) external;
-}
+// @notice Emitted when trying to add an oracle that already exists
+error CollybusDiscountRateRelayer__addOracle_oracleAlreadyRegistered(
+    address oracle
+);
+
+// @notice Emitted when trying to add an oracle for a tokenId that already has a registered oracle.
+error CollybusDiscountRateRelayer__addOracle_tokenIdHasOracleRegistered(
+    uint256 tokenId
+);
 
 contract CollybusDiscountRateRelayer is IRelayer {
     struct OracleData {
         bool exists;
-        uint256 rateId;
+        uint256 tokenId;
         int256 lastUpdateValue;
+        uint256 minimumThresholdValue;
     }
 
-    mapping (address => OracleData) oracles;
+    ICollybus private _collybus;
 
-    function oracleAdd(address oracle_, uint256 rateId_) public {
-        oracles[oracle_] = OracleData({
+    mapping(address => OracleData) private _oracles;
+    mapping(uint256 => bool) private _tokenIdHasOracle;
+    address[] private _oracleAddressIndexes;
+
+    constructor(address collybusAddress_) {
+        _collybus = ICollybus(collybusAddress_);
+    }
+
+    function oracleAdd(
+        address oracle_,
+        uint256 tokenId_,
+        uint256 minimumThresholdValue_
+    ) public {
+        if (oracleExists(oracle_)) {
+            revert CollybusDiscountRateRelayer__addOracle_oracleAlreadyRegistered(
+                oracle_
+            );
+        }
+
+        if (_tokenIdHasOracle[tokenId_]) {
+            revert CollybusDiscountRateRelayer__addOracle_tokenIdHasOracleRegistered(
+                tokenId_
+            );
+        }
+
+        _oracleAddressIndexes.push(oracle_);
+        _tokenIdHasOracle[tokenId_] = true;
+
+        _oracles[oracle_] = OracleData({
             exists: true,
-            rateId: rateId_,
-            lastUpdateValue: 0
+            lastUpdateValue: 0,
+            tokenId: tokenId_,
+            minimumThresholdValue: minimumThresholdValue_
         });
     }
 
-    function oracleExists(address oracle_) public returns (bool) {
-        return oracles[oracle_].exists;
+    function oracleExists(address oracle_) public view returns (bool) {
+        return _oracles[oracle_].exists;
     }
 
-    function check() public returns (bool) {
-        // Check if any of the oracle values differ more than the threshold from the last update
+    function check() external override(IRelayer) returns (bool) {
+        uint256 arrayLength = _oracleAddressIndexes.length;
+        for (uint256 i = 0; i < arrayLength; i++) {
+            IOracle(_oracleAddressIndexes[i]).update();
+            (int256 rate, bool isValid) = IOracle(_oracleAddressIndexes[i])
+                .value();
+            if (!isValid) continue;
+
+            if (
+                absDelta(
+                    _oracles[_oracleAddressIndexes[i]].lastUpdateValue,
+                    rate
+                ) >= _oracles[_oracleAddressIndexes[i]].minimumThresholdValue
+            ) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    function execute() public {
+    function execute() public override(IRelayer) {
         // Update Collybus all tokenIds with the new discount rate
+        uint256 arrayLength = _oracleAddressIndexes.length;
+        for (uint256 i = 0; i < arrayLength; i++) {
+            IOracle(_oracleAddressIndexes[i]).update();
+            (int256 rate, bool isValid) = IOracle(_oracleAddressIndexes[i])
+                .value();
+
+            if (!isValid) continue;
+
+            OracleData memory oracleData = _oracles[_oracleAddressIndexes[i]];
+            if (
+                absDelta(oracleData.lastUpdateValue, rate) >
+                oracleData.minimumThresholdValue
+            ) {
+                oracleData.lastUpdateValue = rate;
+                _collybus.updateDiscountRate(oracleData.tokenId, rate);
+                _oracles[_oracleAddressIndexes[i]] = oracleData;
+            }
+        }
+    }
+
+    function absDelta(int256 a, int256 b) internal pure returns (uint256) {
+        if (a > b) {
+            return uint256(a - b);
+        }
+        return uint256(b - a);
     }
 }
--- src/relayer/CollybusDiscountRate/CollybusDiscountRateRelayer.t.sol
+++ src/relayer/CollybusDiscountRate/CollybusDiscountRateRelayer.t.sol
@@ -4,57 +4,85 @@ pragma solidity ^0.8.0;
 import {DSTest} from "lib/ds-test/src/test.sol";
 import {MockProvider} from "src/test/utils/MockProvider.sol";
 
+import {ICollybus} from "src/relayer/ICollybus.sol";
 import {CollybusDiscountRateRelayer} from "./CollybusDiscountRateRelayer.sol";
+import {IOracle} from "src/oracle/IOracle.sol";
 
-import {Oracle} from "src/oracle/Oracle.sol";
+contract TestCollybus is ICollybus {
+    function updateDiscountRate(uint256 tokenId, int256 rate)
+        external
+        override(ICollybus)
+    {}
+}
 
 contract CollybusDiscountRateRelayerTest is DSTest {
     CollybusDiscountRateRelayer internal cdrr;
+    TestCollybus internal collybus;
 
     function setUp() public {
-        cdrr = new CollybusDiscountRateRelayer();
+        collybus = new TestCollybus();
+        cdrr = new CollybusDiscountRateRelayer(address(collybus));
     }
 
     function test_deploy() public {
-        assertTrue(address(cdrr) != address(0), "CollybusDiscountRateRelayer should be deployed");       
+        assertTrue(
+            address(cdrr) != address(0),
+            "CollybusDiscountRateRelayer should be deployed"
+        );
     }
 
     function test_addOracle_withRateId() public {
         // Create some mock data
         address mockOracle = address(0x123);
         uint256 mockRateId = 0x456;
+        uint256 rateMinThreshold = 0;
 
         // Add oracle with rate id
-        cdrr.oracleAdd(mockOracle, mockRateId);
+        cdrr.oracleAdd(mockOracle, mockRateId, rateMinThreshold);
 
         // Check that oracle was added
         assertTrue(cdrr.oracleExists(mockOracle), "Oracle should be added");
     }
 
     function test_ReturnNumberOfOracles() public {}
+
     function testFail_AddOracle_ShouldNotAllowDuplicates() public {}
+
     function test_AddOracle_OnlyAuthorizedUserShouldBeAbleToAdd() public {}
+
     function test_CheckExistenceOfOracle() public {}
+
     function test_RemoveOracle_DeletesOracle() public {}
+
     function testFail_RemoveOracle_ShouldFailIfOracleDoesNotExist() public {}
-    function test_RemoveOracle_OnlyAuthorizedUserShouldBeAbleToRemove() public {}
+
+    function test_RemoveOracle_OnlyAuthorizedUserShouldBeAbleToRemove()
+        public
+    {}
 
     function test_checkCallsUpdate_onAllOracles() public {
         // Create an oracle with an associated rateId
         MockProvider oracle1 = new MockProvider();
         uint256 rateId1 = 0x1;
+        uint256 rateMinThreshold1 = 0;
 
         // Add the oracle
-        cdrr.oracleAdd(address(oracle1), rateId1);
+        cdrr.oracleAdd(address(oracle1), rateId1, rateMinThreshold1);
 
         cdrr.check();
 
         // Check that the oracle was called correctly
         MockProvider.CallData memory cd = oracle1.getCallData(0);
-        assertEq(cd.caller, address(cdrr), "Caller should be CollybusDiscountRateRelayer");
-        assertEq(cd.functionSelector, Oracle.update.selector);
-        assertEq(keccak256(cd.data), keccak256(abi.encodeWithSelector(Oracle.update.selector)));
+        assertEq(
+            cd.caller,
+            address(cdrr),
+            "Caller should be CollybusDiscountRateRelayer"
+        );
+        assertEq(cd.functionSelector, IOracle.update.selector);
+        assertEq(
+            keccak256(cd.data),
+            keccak256(abi.encodeWithSelector(IOracle.update.selector))
+        );
         assertEq(cd.value, 0);
     }
-
 }
