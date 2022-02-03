58db299852f76659b07bc1f22d7b6d3ff86e866f
Alexandru Marcu <alx.marcu@gmail.com>
2022-02-03 12:51:24 +0200

    Create goerli_deploy.js

---
+++ scripts/goerli_deploy.js
@@ -0,0 +1,27 @@
+const ethers = require('ethers');
+const { getContractFactory, deployContract } = require('./common.js');
+
+ethers.utils.Logger.setLogLevel(ethers.utils.Logger.levels.ERROR);
+const { parseEther: toWei, formatBytes32String: toBytes32 } = ethers.utils;
+
+// requires setting ETH_FROM_KEY and ETH_RPC_URL and a running instance of a network
+(async function () {
+    const deployer = new ethers.Wallet(process.env.ETH_FROM_KEY, new ethers.providers.JsonRpcProvider(process.env.ETH_RPC_URL));
+    const me = await deployer.getAddress();
+
+    // Deploy element finance value provider
+    // PoolID: 0x4294005520c453eb8fa66f53042cfc79707855c400020000000000000000009a
+    // Balancer Vault: 0x65748E8287Ce4B9E6D83EE853431958851550311
+    // Underlier : 0x78dEca24CBa286C0f8d56370f5406B48cFCE2f86
+    // Pt Bond : 0xDCf80C068B7fFDF7273d8ADAE4B076BF384F711A
+    // Time to maturity: 1660165080
+    // Unit Seconds: 284012568
+
+    console.log("START");
+
+    const elementValueProvider = await deployContract('GoerliDeploy', 
+        await getContractFactory('src/deploy/GoerliDeploy.sol', 'GoerliDeploy', deployer)
+    );
+
+    console.log("END");
+})();
