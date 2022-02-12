c80c0e8fa697915bfad4d8a0cd0bac8de1d16b79
Daniel Luca <lucadanielcostin@gmail.com>
2022-02-12 14:51:37 -0700

    👨‍❤️‍💋‍👨 Add test for permission check

--- src/factory/FactoryAggregatorOracle.t.sol
+++ src/factory/FactoryAggregatorOracle.t.sol
@@ -7,6 +7,8 @@ import {MockProvider} from "@cleanunicorn/mockprovider/src/MockProvider.sol";
 
 import {FactoryAggregatorOracle} from "src/factory/FactoryAggregatorOracle.sol";
 
+import {AggregatorOracle} from "src/aggregator/AggregatorOracle.sol";
+
 contract FactoryAggregatorOracleTest is DSTest {
     FactoryAggregatorOracle private _factory;
 
@@ -24,6 +26,15 @@ contract FactoryAggregatorOracleTest is DSTest {
         assertTrue(
             oracleAddress != address(0),
             "Factory Aggregator Oracle create failed"
+        );
+    }
+
+    function test_create_AddsPermission_OnSender() public {
+        address oracle = _factory.create();
+
+        assertTrue(
+            AggregatorOracle(oracle).canCall(AggregatorOracle(oracle).ANY_SIG(), address(this)),
+            "Creator shold have admin access"
         );
     }
 }
