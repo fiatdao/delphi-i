e34ebd8fe74ca5d5273c0bb0815c9d2f5ebe89d1
Daniel Luca <lucadanielcostin@gmail.com>
2022-02-24 11:56:40 -0700

    👨‍👧 Update dapptools version

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   pkgs = import (builtins.fetchGit rec {
     name = "dapptools-${rev}";
     url = https://github.com/dapphub/dapptools;
-    rev = "adcc076b1441b3a928a8f0b42b2a63f05d9bcf0d";
+    rev = "fb9476ded759da44c449eb391cc67bfb0df61112";
   }) {};
 
 in
