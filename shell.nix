c6027e04612b695cd38e54a843635f55588c179e
Daniel Luca <lucadanielcostin@gmail.com>
2022-03-04 08:52:34 +0200

    🌊 Upgrade Solidity to 0.8.12

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   pkgs = import (builtins.fetchGit rec {
     name = "dapptools-${rev}";
     url = https://github.com/dapphub/dapptools;
-    rev = "1be7d796a468f52a5eb5b6830591d76a3b4b1c49";
+    rev = "42a807306188cd20b1fe925ffe469979e2802b82";
   }) {};
 
 in
