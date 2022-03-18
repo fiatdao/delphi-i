98a0201390af7e3438fcc794f2289683c3798d52
Alexandru Marcu <alx.marcu@gmail.com>
2022-03-18 18:27:19 +0200

    Updated Relayer.
    Removed multiple Oracle support.

--- src/relayer/IRelayer.sol
+++ src/relayer/IRelayer.sol
@@ -10,18 +10,4 @@ interface IRelayer {
     function execute() external returns (bool);
 
     function executeWithRevert() external;
-
-    function oracleCount() external view returns (uint256);
-
-    function oracleAdd(
-        address oracle_,
-        bytes32 encodedToken_,
-        uint256 minimumPercentageDeltaValue_
-    ) external;
-
-    function oracleRemove(address oracle_) external;
-
-    function oracleExists(address oracle_) external view returns (bool);
-
-    function oracleAt(uint256 index_) external view returns (address);
 }
--- src/relayer/Relayer.sol
+++ src/relayer/Relayer.sol
@@ -8,25 +8,6 @@ import {ICollybus} from "./ICollybus.sol";
 import {Guarded} from "../guarded/Guarded.sol";
 
 contract Relayer is Guarded, IRelayer {
-    // @notice Emitted when trying to add an oracle that already exists
-    error Relayer__addOracle_oracleAlreadyRegistered(
-        address oracle,
-        RelayerType relayerType
-    );
-
-    // @notice Emitted when trying to add an oracle for a tokenId that already has a registered oracle.
-    error Relayer__addOracle_tokenIdHasOracleRegistered(
-        address oracle,
-        bytes32 tokenId,
-        RelayerType relayerType
-    );
-
-    // @notice Emitter when trying to remove an oracle that was not registered.
-    error Relayer__removeOracle_oracleNotRegistered(
-        address oracle,
-        RelayerType relayerType
-    );
-
     // @notice Emitter when execute() does not update any oracle
     error Relayer__executeWithRevert_noUpdates(RelayerType relayerType);
 
@@ -34,217 +15,70 @@ contract Relayer is Guarded, IRelayer {
     //         The Relayer needs to be able to call Update on all Oracles
     error Relayer__unauthorizedToCallUpdateOracle(address oracleAddress);
 
-    struct OracleData {
-        bool exists;
-        bytes32 tokenId;
-        int256 lastUpdateValue;
-        uint256 minimumPercentageDeltaValue;
-    }
-
-    /// ======== Events ======== ///
-
-    event OracleAdded(address oracleAddress);
-    event OracleRemoved(address oracleAddress);
-    event ShouldUpdate(bool shouldUpdate);
     event UpdateOracle(address oracle, int256 value, bool valid);
     event UpdatedCollybus(bytes32 tokenId, uint256 rate, RelayerType);
 
     /// ======== Storage ======== ///
 
     address public immutable collybus;
-
     RelayerType public immutable relayerType;
+    address public immutable oracle;
+    bytes32 public immutable encodedTokenId;
+    uint256 public minimumPercentageDeltaValue;
 
-    // Mapping that will hold all the oracle params needed by the contract
-    mapping(address => OracleData) private _oraclesData;
-
-    // Mapping used tokenId's
-    mapping(bytes32 => bool) public encodedTokenIds;
-
-    // Array used for iterating the oracles.
-    using EnumerableSet for EnumerableSet.AddressSet;
-    EnumerableSet.AddressSet private _oracleList;
-
-    constructor(address collybusAddress_, RelayerType type_) {
+    uint256 private _lastUpdateValue;
+    
+    constructor(address collybusAddress_, RelayerType type_, address oracleAddress_, bytes32 encodedTokenId_, uint256 minimumPercentageDeltaValue_) {
         collybus = collybusAddress_;
         relayerType = type_;
-    }
-
-    /// @notice Returns the number of registered oracles.
-    /// @return the total number of oracles.
-    function oracleCount() external view override(IRelayer) returns (uint256) {
-        return _oracleList.length();
-    }
-
-    /// @notice Returns the address of an oracle at index
-    /// @dev Reverts if the index is out of bounds
-    /// @param index_ The internal index of the oracle
-    /// @return Returns the address pf the oracle
-    function oracleAt(uint256 index_)
-        external
-        view
-        override(IRelayer)
-        returns (address)
-    {
-        return _oracleList.at(index_);
-    }
-
-    /// @notice Checks whether an oracle is registered.
-    /// @param oracle_ The address of the oracle.
-    /// @return Returns 'true' if the oracle is registered.
-    function oracleExists(address oracle_)
-        public
-        view
-        override(IRelayer)
-        returns (bool)
-    {
-        return _oraclesData[oracle_].exists;
-    }
-
-    /// @notice Registers an oracle to a token id and set the minimum threshold delta value
-    /// calculate the annual rate.
-    /// @param oracle_ The address of the oracle.
-    /// @param encodedTokenId_ The unique token id for which this oracle will update rate values.
-    /// @param minimumPercentageDeltaValue_ The minimum value delta threshold needed in order to push values to the Collybus
-    /// @dev Reverts if the oracle is already registered or if the rate id is taken by another oracle.
-    function oracleAdd(
-        address oracle_,
-        bytes32 encodedTokenId_,
-        uint256 minimumPercentageDeltaValue_
-    ) public override(IRelayer) checkCaller {
-        if (!Guarded(oracle_).canCall(IOracle.update.selector, address(this))) {
-            revert Relayer__unauthorizedToCallUpdateOracle(oracle_);
-        }
-
-        // Make sure the oracle was not added previously
-        if (oracleExists(oracle_)) {
-            revert Relayer__addOracle_oracleAlreadyRegistered(
-                oracle_,
-                relayerType
-            );
-        }
-
-        // Make sure there are no existing oracles registered for this rate Id
-        if (encodedTokenIds[encodedTokenId_]) {
-            revert Relayer__addOracle_tokenIdHasOracleRegistered(
-                oracle_,
-                encodedTokenId_,
-                relayerType
-            );
-        }
-
-        // Add oracle in the oracle address array that is used for iterating.
-        _oracleList.add(oracle_);
-
-        // Mark the token Id as used
-        encodedTokenIds[encodedTokenId_] = true;
-
-        // Update the oracle address => data mapping with the oracle parameters.
-        _oraclesData[oracle_] = OracleData({
-            exists: true,
-            lastUpdateValue: 0,
-            tokenId: encodedTokenId_,
-            minimumPercentageDeltaValue: minimumPercentageDeltaValue_
-        });
-
-        emit OracleAdded(oracle_);
-    }
-
-    /// @notice Unregisters an oracle.
-    /// @param oracle_ The address of the oracle.
-    /// @dev Reverts if the oracle is not registered
-    function oracleRemove(address oracle_)
-        public
-        override(IRelayer)
-        checkCaller
-    {
-        // Make sure the oracle is registered
-        if (!oracleExists(oracle_)) {
-            revert Relayer__removeOracle_oracleNotRegistered(
-                oracle_,
-                relayerType
-            );
-        }
-
-        // Reset the tokenId Mapping
-        encodedTokenIds[_oraclesData[oracle_].tokenId] = false;
-
-        // Remove the oracle from the list
-        // This returns true/false depending on if the oracle was removed
-        _oracleList.remove(oracle_);
-
-        // Reset struct to default values
-        delete _oraclesData[oracle_];
-
-        emit OracleRemoved(oracle_);
-    }
-
-    /// @notice Returns the oracle data for a given oracle address
-    /// @param oracle_ The address of the oracle
-    /// @return Returns the oracle data as `OracleData`
-    function oraclesData(address oracle_)
-        public
-        view
-        returns (OracleData memory)
-    {
-        return _oraclesData[oracle_];
+        oracle = oracleAddress_;
+        encodedTokenId = encodedTokenId_;
+        minimumPercentageDeltaValue = minimumPercentageDeltaValue_;
+        _lastUpdateValue = 0;
     }
 
     /// @notice Iterates and updates all the oracles and pushes the updated data to Collybus for the
     /// oracles that have delta changes in value bigger than the minimum threshold values.
     /// @dev Oracles that return invalid values are skipped.
     function execute() public override(IRelayer) checkCaller returns (bool) {
-        bool updated;
+        // We always update the oracles before retrieving the rates
+        bool oracleUpdated = IOracle(oracle).update();
+        (int256 oracleValue, bool isValid) = IOracle(oracle).value();
 
-        // Update Collybus all tokenIds with the new discount rate
-        uint256 arrayLength = _oracleList.length();
-        for (uint256 i = 0; i < arrayLength; i++) {
-            // Cache oracle address
-            address localOracle = _oracleList.at(i);
-
-            // We always update the oracles before retrieving the rates
-            bool oracleUpdated = IOracle(localOracle).update();
-            if (oracleUpdated) {
-                updated = true;
-            }
-            (int256 oracleValue, bool isValid) = IOracle(localOracle).value();
-
-            if (!isValid) continue;
-
-            OracleData storage oracleData = _oraclesData[localOracle];
-
+        if(oracleUpdated && isValid){
             // If the change in delta rate from the last update is bigger than the threshold value push
             // the rates to Collybus
             if (
                 checkDeviation(
-                    oracleData.lastUpdateValue,
+                    _lastUpdateValue,
                     oracleValue,
-                    oracleData.minimumPercentageDeltaValue
+                    minimumPercentageDeltaValue
                 )
             ) {
-                oracleData.lastUpdateValue = oracleValue;
+                _lastUpdateValue = oracleValue;
 
                 if (relayerType == RelayerType.DiscountRate) {
                     ICollybus(collybus).updateDiscountRate(
-                        uint256(oracleData.tokenId),
+                        uint256(encodedTokenId),
                         uint256(oracleValue)
                     );
                 } else if (relayerType == RelayerType.SpotPrice) {
                     ICollybus(collybus).updateSpot(
-                        address(uint160(uint256(oracleData.tokenId))),
+                        address(uint160(uint256(encodedTokenId))),
                         uint256(oracleValue)
                     );
                 }
 
                 emit UpdatedCollybus(
-                    oracleData.tokenId,
+                    encodedTokenId,
                     uint256(oracleValue),
                     relayerType
                 );
             }
+
         }
 
-        return updated;
+        return oracleUpdated;
     }
 
     /// @notice The function will call `execute()` and will revert if no oracle was updated
